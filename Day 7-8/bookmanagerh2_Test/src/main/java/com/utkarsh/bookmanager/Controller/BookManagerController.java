844bd2661cfddd1c60ada7097c6a9537b4e62e51
UtkarshP25 <utkarsh2002pawar@gmail.com>
2025-06-24 03:07:13 +0530

    Added Day 7 & 8 - JUnit + Integration Testing with MockMvc

---
+++ Day 7-8/bookmanagerh2_Test/src/main/java/com/utkarsh/bookmanager/Controller/BookManagerController.java
@@ -0,0 +1,47 @@
+package com.utkarsh.bookmanager.Controller;
+
+import com.utkarsh.bookmanager.Entity.Book;
+import com.utkarsh.bookmanager.Service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookManagerController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/id/{myid}")
+    public Book getBookById(@PathVariable int myid) {
+
+        return bookService.getBookById(myid);
+    }
+
+    @PostMapping
+    public String addBook(@Valid @RequestBody Book book) {
+       bookService.addBook(book);
+       return "Book added successfully";
+    }
+
+    @PutMapping("/id/{id}")
+    public void updateBook(@PathVariable int id, @Valid @RequestBody Book book) {
+        bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("id/{myid}")
+    public void deleteBook(@PathVariable int myid) {
+        bookService.deleteBook(myid);
+    }
+
+}
---
+++ Day 7-8/bookmanagerh2_Test/src/test/java/com/utkarsh/bookmanager/BookmanagerApplicationH2AndTestTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerApplicationH2AndTestTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
